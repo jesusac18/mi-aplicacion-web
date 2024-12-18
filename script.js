6ba7b188083d3b8cbc1335654e2fe4b4a15b86f6
jesusac18 <jesusaguileracampoy10@gmail.com>
2024-12-18 08:59:59 +0100

    Tema Claro y Tema Osuro
    Boton de tema Claro y tema Oscuro

---
+++ script.js
@@ -0,0 +1,18 @@
+const button = document.querySelector("button")
+const h1 = document.querySelector("h1")
+const body = document.querySelector("body")
+let boolean = true
+
+button.addEventListener("click",function () {
+    if (boolean) {
+        h1.style.color = 'white'
+        body.style.backgroundColor = 'black'
+        button.textContent = 'Tema Claro'
+        boolean = !boolean
+    } else {
+        h1.style.color = 'black'
+        body.style.backgroundColor = 'white'
+        button.textContent = 'Tema Osuro'
+        boolean = !boolean
+    }
+})
